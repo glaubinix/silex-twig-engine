4a4b71ae6dfa9442c68ec42c9140f8d261577245
Stephan Vock <stephan.vock@gmail.com>
2016-05-24 00:55:05 +0200

    Test to assert service is correctly initialized

--- tests/TwigEngineServiceProviderTest.php
+++ tests/TwigEngineServiceProviderTest.php
@@ -2,7 +2,22 @@
 
 namespace Glaubinix\Tests\TwigEngine;
 
+use Glaubinix\TwigEngine\TwigEngineServiceProvider;
+use Pimple\Container;
+use Silex\Provider\TwigServiceProvider;
+use Symfony\Bridge\Twig\TwigEngine;
+
 class TwigEngineServiceProviderTest extends \PHPUnit_Framework_TestCase
 {
-    
+    public function testCreateTemplating()
+    {
+        $pimple = new Container([
+            'debug' => false,
+            'charset' => '',
+        ]);
+        $pimple->register(new TwigEngineServiceProvider());
+        $pimple->register(new TwigServiceProvider());
+
+        $this->assertInstanceOf(TwigEngine::class, $pimple['templating']);
+    }
 }
